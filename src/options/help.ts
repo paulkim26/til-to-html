6f2f36bb4b54fe7a20f0208b085da57c65fc4f56
Paul Kim <paulkim26.pk@gmail.com>
2023-09-15 21:26:54 -0400

    feat: made help message more verbose

--- src/options/help.ts
+++ src/options/help.ts
@@ -3,9 +3,21 @@ import getPackageInfo from "@/helpers/getPackageInfo";
 // Prints usage message
 export default async function help() {
   const packageInfo = await getPackageInfo();
-
   const tool = packageInfo.name;
-  console.log(
-    `${tool} [-h | --help] [-v | --version] [-o | --output output] filename | directory`
-  );
+
+  let message = "";
+  message += `Usage: ${tool} [OPTIONS] [ARGUMENT]\n\n`;
+  message += `Description: ${tool} is a tool that converts TIL Markdown posts into static HTML pages.\n\n`;
+
+  message += `Options:\n`;
+  message += `\t-h, --help\t\tDisplay usage.\n`;
+  message += `\t-v, --version\t\tDisplay version.\n`;
+  message += `\t-o, --output DIRECTORY\tIndicate a custom output directory.\n`;
+  message += `\n`;
+
+  message += `Arguments:\n`;
+  message += `\tTARGET\t\t\tEither a single input text file or folder to convert.`;
+  message += `\n\n`;
+
+  console.log(message);
 }
