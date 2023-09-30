70046c1fd809e0ba1f3e171cebbf204b6ab7b461
Paul Kim <paulkim26.pk@gmail.com>
2023-09-29 22:14:47 -0400

    feat: exit with non-zero exit code if error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import parseArguments from "~/parseArguments";
 const args = Bun.argv.slice(2);
 try {
   await parseArguments(args);
+  process.exit(0);
 } catch (e: any) {
   console.error(e.message);
+  process.exit(1);
 }
